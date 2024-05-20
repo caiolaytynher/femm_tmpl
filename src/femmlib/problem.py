3d9e3ad626b7e3b70ddbaf05e50ac3e40c2febe2
caiolaytynher <csrlaytynher@proton.me>
2024-05-20 07:25:25 -0300

    improoved problem architecture

--- src/femmlib/problem.py
+++ src/femmlib/problem.py
@@ -1,11 +1,10 @@
 from dataclasses import dataclass
+from pathlib import Path
 from typing import Literal, Self
 
 import femm
 
-from femmlib.unit import Name as UnitName
-from femmlib.unit import Unit
-from helpers.path import PathLike, parse_path
+from femmlib.unit import Unit, UnitLike
 
 type DocType = Literal[
     'magnetics',
@@ -17,8 +16,56 @@ type ProblemType = Literal['planar', 'axi']
 type ArcSolver = Literal['successive approximation', 'newton']
 
 
-@dataclass(init=False)
+@dataclass
 class Problem:
+    freq: int
+    unit: Unit
+    type: ProblemType
+    precision: float
+    depth: int
+    min_angle: int
+    arc_solver: ArcSolver
+
+    def update(self) -> Self:
+        femm.mi_probdef(
+            self.freq,
+            self.unit.name,
+            self.type,
+            self.precision,
+            self.depth,
+            self.min_angle,
+            0 if self.arc_solver == 'successive approximation' else 1,
+        )
+
+        return self
+
+    @staticmethod
+    def solve(file_name: str) -> None:
+        """
+        Salva o arquivo no caminho definido por `file`, criando a pasta se
+        necessário, cria a mecha, analisa o circuito e carrega a solução.
+        Certifique-se que o arquivo termina em `.FEM`.
+        """
+        if not file_name.endswith('.FEMM'):
+            file_name += '.FEMM'
+
+        folder = Path('femm').resolve()
+        if not folder.exists():
+            folder.mkdir()
+
+        # Salva, cria a mecha e analisa a solução.
+        femm.mi_saveas(str(folder / file_name))
+        femm.mi_createmesh()
+        femm.mi_analyze()
+        femm.mi_loadsolution()
+
+    def update_freq(self, freq: int) -> Self:
+        self.freq = freq
+        return self.update()
+
+
+@dataclass(init=False)
+class ProblemBuilder:
     """
     Responsável por definir, salvar e resolver o problema. Os valores
     padrão são os mesmos do FEMM. Ao instanciar esta classe, um novo
@@ -26,14 +73,9 @@ class Problem:
     definidas.
     """
 
-    type: ProblemType
-    freq: int
-    unit: Unit
-    depth: int
-
     def __init__(
         self,
-        doc_type: Literal[DocType],
+        doc_type: DocType,
     ) -> None:
         """
         Tipo de documeto.
@@ -41,44 +83,47 @@ class Problem:
         `'magnetics'` : magnético
         `'electrostatics'` : eletrostático
         `'heat flow'` : fluxo de calor
-        `'current flow'` : fluxo de corrente.
+        `'current flow'` : fluxo de corrente
         """
-        match doc_type:
-            case 'magnetics':
-                self.doc_type = 0
-            case 'electrostatics':
-                self.doc_type = 1
-            case 'heat flow':
-                self.doc_type = 2
-            case 'current flow':
-                self.doc_type = 3
-
+        self.doc_type: DocType = doc_type
         self.freq: int = 0
         self.unit = Unit('inches')
-        self.problem_type: ProblemType = 'planar'
+        self.type: ProblemType = 'planar'
         self.precision: float = 1e-8
         self.depth: int = 1
         self.min_angle: int = 30
-        self.arc_solver: Literal[0, 1] = 0
+        self.arc_solver: ArcSolver = 'successive approximation'
 
     def with_freq(self, freq: int) -> Self:
         """Frequência."""
         self.freq = freq
         return self
 
-    def with_unit(self, unit: UnitName) -> Self:
+    def with_unit(self, unit: UnitLike) -> Self:
         """
         Unidade de medida das distâncias.
 
-        "inches", "millimeters", "centimeters",
-        "mils" e "meters".
+        `'inches'` : polegadas
+        `'millimeters'` : milímetros
+        `'centimeters'` : centímetros
+        `'mils'` : mils (milésimo de polegada)
+        `'meters'` : metros
         """
-        self.unit = unit
+        if isinstance(unit, Unit):
+            self.unit = unit
+        else:
+            self.unit = Unit(unit)
+
         return self
 
-    def with_type(self, problem_type: ProblemType) -> Self:
-        """Tipo do problema. "planar" para 2D e "axi" para 3D."""
-        self.problem_type = problem_type
+    def with_type(self, type: ProblemType) -> Self:
+        """
+        Tipo do problema.
+
+        `'planar'` : 2D
+        `'axi'` : 3D
+        """
+        self.type = type
         return self
 
     def with_precision(self, precision: float) -> Self:
@@ -98,53 +143,37 @@ class Problem:
 
     def with_arc_solver(self, arc_solver: ArcSolver) -> Self:
         """
-        Método de resolução dos arcos. 0 para aproximação sucessiva e 1
-        para Newton.
+        Método de resolução dos arcos.
+
+        `'succesive approximation'`: aproximações sucessivas
+        `'newton'` : método de Newton
         """
         self.arc_solver = arc_solver
         return self
 
-    def build(self) -> tuple[Problem, Unit]:
+    def build(self):
         """
         Abre um novo documento no modo definido por `Problem.doctype` e
         define as variáveis do problema.
         """
-        femm.newdocument(self.doc_type)
-        femm.mi_probdef(
+        match self.doc_type:
+            case 'magnetics':
+                femm.newdocument(0)
+            case 'electrostatics':
+                femm.newdocument(1)
+            case 'heat flow':
+                femm.newdocument(2)
+            case 'current flow':
+                femm.newdocument(3)
+
+        problem = Problem(
             self.freq,
             self.unit,
-            self.problem_type,
+            self.type,
             self.precision,
             self.depth,
             self.min_angle,
             self.arc_solver,
-        )
+        ).update()
 
-        return Problem(self.freq, self.problem_type, self.depth), Unit(
-            self.unit
-        )
-
-
-def builder() -> Builder:
-    """
-    Recebe o nome do arquivo .FEM e caminho de diretório onde serão
-    salvos os arquivos específicos do FEMM. Se colocar uma pasta que não
-    existe antes do nome do arquivo, a pasta será criada.
-    """
-    return Builder()
-
-
-def solve(file: PathLike) -> None:
-    """
-    Salva o arquivo no caminho definido por `file`, criando a pasta se
-    necessário, cria a mecha, analisa o circuito e carrega a solução.
-    Certifique-se que o arquivo termina em `.FEM`.
-    """
-    file = parse_path(file, ensure_parent=True)
-    assert file.name.endswith('.FEM'), f'File {file} should end in .FEM'
-
-    # Salva, cria a mecha e analisa a solução.
-    femm.mi_saveas(str(file))
-    femm.mi_createmesh()
-    femm.mi_analyze()
-    femm.mi_loadsolution()
+        return problem
