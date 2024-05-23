0d4dde0a19c1c4f4e4b6bace691508f2a48d1ee0
caiolaytynher <csrlaytynher@proton.me>
2024-05-22 23:59:26 -0300

    new entrypoint

--- src/main.py
+++ src/main.py
@@ -1,17 +1,51 @@
-from femmlib import problem
-from femmlib.core import open_femm
+from collections.abc import Callable
 
-# TODO: Não precisa especificar a pasta pra tudo, é bom definir um
-# standard, até pra facilitar a limpeza dos dados com o make.
+from femmlib.core import FEMM
 
 
 def main() -> None:
-    with open_femm(delay=2):
-        state, unit = (
-            problem.builder().with_depth(2).with_unit('centimeters').build()
+    app = FEMM(
+        doc_type='magnetics',
+        freq=60,
+        unit='centimeters',
+        depth=2,
+    )
+
+    with app.new('base.FEM'):
+        circle = app.circle((0, 0), 5)
+
+    def analyze() -> None:
+        with app.open('base.FEM', delay=20):
+            circle.select('arc')
+
+    interactive_prompt(analyze)
+
+
+def interactive_prompt(*commands: Callable[[], None]) -> None:
+    while True:
+        print(
+            'Options:',
+            *[
+                f'\t{i + 1}. {cmd.__name__.capitalize().replace('_', ' ')}'
+                for i, cmd in enumerate(commands)
+            ],
+            '\tQ/q. Quit',
+            sep='\n',
         )
-        print(state, unit)
-        problem.solve('femm/tmp.FEM')
+        command = input('> ')
+
+        if command.lower() == 'q':
+            break
+
+        if not command.isdigit():
+            continue
+
+        command_index = int(command) - 1
+
+        if command_index > len(commands) - 1 or command_index < 0:
+            continue
+
+        commands[command_index]()
 
 
 if __name__ == '__main__':
